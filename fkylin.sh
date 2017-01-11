ac5b802eb80117c6f76171d93fd4955834c3871e
bugzhang <flyhighdream@qq.com>
2017-01-11 23:32:23 +0800

    no message

--- fkylin.sh
+++ fkylin.sh
@@ -2,3 +2,16 @@
 
 
 # install jdk
+apt-get update
+apt-get install openjdk-7-jre openjdk-7-jdk
+
+#install hadoop
+cd /usr/local
+
+hadoop-1.2.1_path = http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz
+hadoop-2.7.2_path = http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+hadoop-3.0.0-alphal_path = http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.0.0-alpha1/hadoop-3.0.0.tar.gz
+
+mirror
+mirror
+wget
