cc950f86d733691b8d036dce2952cb096ac3007e
bugzhang <flyhighdream@qq.com>
2017-01-12 21:44:55 +0800

    no message

--- fkylin.sh
+++ fkylin.sh
@@ -8,27 +8,19 @@ apt-get install openjdk-7-jre openjdk-7-jdk
 yum update
 yum install java-1.8.0-openjdk-devel.x86_64
 
-#install hadoop
+# install hadoop
 cd /usr/local
-echo "Which hadoop do you want need?(Please input 1,2,3, default 2)"
-echo "1:hadoop-1.2.1 : 早期版本"
-echo "2:hadoop-2.7.2 : 默认版本"
-echo "3:hadoop-3.0.0(alphal) : 最新试用版本"
-hadoop_url = "http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz"
-hadoop-1.2.1_path = "http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz"
-hadoop-2.6.5_path = "http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz"
-hadoop-3.0.0-alphal_path = "http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-3.0.0-alpha1/hadoop-3.0.0.tar.gz"
-read hadoop
-if [ $hadoop = 1 ]
-then
-   $hadoop_url = ${hadoop-1.2.1_path}
-elif [ $hadoop = 2 ]
-then
-   $hadoop_url = ${hadoop-2.7.2_path}
-elif [ $hadoop = 3 ]
-then
-   $hadoop_url = ${hadoop-3.0.0-alphal_path}
-else
-   $hadoop_url = ${hadoop-2.7.2_path}
-fi
-wget ${hadoop_url}
+wget http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
+tar -zxvf hadoop-2.6.5.tar.gz
+
+
+# install hbase
+wget http://mirror.bit.edu.cn/apache/hbase/1.1.8/hbase-1.1.8-bin.tar.gz
+tar -zxvf hbase-1.1.8-bin.tar.gz
+
+
+#install hive
+
+
+
+#install kylin
