5e05834e47654e9b5f3159d1e6858903ac0f05e7
bugzhang <flyhighdream@qq.com>
2017-01-15 02:37:44 +0800

    no message

--- fkylin.py
+++ fkylin.py
@@ -1,24 +1,32 @@
 import os
 
-print "------------------------------------"
+print "---------------------------------------------"
 print "welcome to use fkylin script,this script can auto install java1.8,hadoop2.6.5,hbase1.1.8,hive2.0.1,kylin1.5.3 in linux"
 print "write by bugzhang"
 print "github-https://github.com/rhythm1995/fkylin"
-print "------------------------------------"
+print "______ _          _ _       "
+print "|  ___| |        | (_)      "
+print "| |_  | | ___   _| |_ _ __  "
+print "|  _| | |/ / | | | | | '_ \ "
+print "| |   |   <| |_| | | | | | |"
+print "\_|   |_|\_\\__, |_|_|_| |_|"
+print "             __/ |          "
+print "            |___/           "
+print "---------------------------------------------"
 
 print "Which linux release version do you use?(1 or 2 or 3)"
+print "0.auto get(form /etc/issue)"
 print "1.centos6+"
 print "2.ubuntu14.4+"
 print "3.debian"
 print "we can not support other release version"
+
 linux_release_version = raw_input()
 if (linux_release_version != 1) or (linux_release_version != 2) or (linux_release_version != 3):
     print  "please input 1 or 2 or 3"
 
 if linux_release_version == 1:
-    centos()
-
-def centos():
+    print "Start install in Centos"
     os.system("yum update")
     os.system("yum install java-1.8.0-openjdk-devel.x86_64")
     os.system("wget http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-/2.6.5/hadoop-2.6.5.tar.gz")
