a3d875980a903e9b4b4a854bd35278ddcb381332
bugzhang <flyhighdream@qq.com>
2017-01-15 03:04:36 +0800

    no message

--- fkylin.py
+++ fkylin.py
@@ -15,11 +15,16 @@ print "            |___/           "
 print "---------------------------------------------"
 
 print "Which linux release version do you use?(1 or 2 or 3)"
-print "0.auto get(form /etc/issue)"
-print "1.centos6+"
-print "2.ubuntu14.4+"
-print "3.debian"
+print "0.Auto get(form /etc/issue)"
+print "1.centos 6+"
+print "2.ubuntu 14.4+"
+print "3.debian 8+"
 print "we can not support other release version"
+
+
+etc_issue = open('/etc/issue')
+linux_release_etc = etc_issue.readline()
+
 
 linux_release_version = raw_input()
 if (linux_release_version != 1) or (linux_release_version != 2) or (linux_release_version != 3):
@@ -33,3 +38,12 @@ if linux_release_version == 1:
     os.system("wget http://mirror.bit.edu.cn/apache/hbase/1.1.8/hbase-1.1.8-bin.tar.gz")
     os.system("wget http://mirror.bit.edu.cn/apache/hive/hive-2.0.1/apache-hive-2.0.1-bin.tar.gz")
     os.system("wget http://mirror.bit.edu.cn/apache/kylin/apache-kylin-1.5.3/apache-kylin-1.5.3-bin.tar.gz")
+
+os.system("tar -zxvf hadoop-2.6.5.tar.gz")
+os.system("tar -zxvf hbase-1.1.8-bin.tar.gz")
+os.system("tar -zxvf apache-hive-2.0.1-bin.tar.gz")
+os.system("apache-kylin-1.5.3-bin.tar.gz")
+os.system("mv hadoop-2.6.5.tar.gz hadoop")
+os.system("mv apache-hive-2.0.1-bin.tar.gz hive")
+os.system("mv hbase-1.1.8-bin.tar.gz")
+os.system("mv apache-kylin-1.5.3-bin.tar.gz kylin")
