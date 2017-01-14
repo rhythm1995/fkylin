98dbe9aee174339777074aab7dd11a0ff747f9e7
bugzhang <flyhighdream@qq.com>
2017-01-15 03:14:45 +0800

    no message

--- fkylin.py
+++ fkylin.py
@@ -24,7 +24,13 @@ print "we can not support other release version"
 
 etc_issue = open('/etc/issue')
 linux_release_etc = etc_issue.readline()
-
+linux_release_key = linux_release_etc[1]
+if linux_release_key == 'C':
+    linux_release_version = 1
+elif linux_release_key == 'U':
+    linux_release_version = 2
+elif linux_release_key == 'D':
+    linux_release_version = 3
 
 linux_release_version = raw_input()
 if (linux_release_version != 1) or (linux_release_version != 2) or (linux_release_version != 3):
@@ -34,6 +40,7 @@ if linux_release_version == 1:
     print "Start install in Centos"
     os.system("yum update")
     os.system("yum install java-1.8.0-openjdk-devel.x86_64")
+    os.system("cd /usr/local")
     os.system("wget http://mirror.bit.edu.cn/apache/hadoop/common/hadoop-/2.6.5/hadoop-2.6.5.tar.gz")
     os.system("wget http://mirror.bit.edu.cn/apache/hbase/1.1.8/hbase-1.1.8-bin.tar.gz")
     os.system("wget http://mirror.bit.edu.cn/apache/hive/hive-2.0.1/apache-hive-2.0.1-bin.tar.gz")
@@ -47,3 +54,15 @@ os.system("mv hadoop-2.6.5.tar.gz hadoop")
 os.system("mv apache-hive-2.0.1-bin.tar.gz hive")
 os.system("mv hbase-1.1.8-bin.tar.gz")
 os.system("mv apache-kylin-1.5.3-bin.tar.gz kylin")
+
+print "---------------------------------------------"
+print " congratulation!You finish all install,use commend './usr/local/kylin/bin/kylin.sh start' woo start kylin"
+print "______ _          _ _       "
+print "|  ___| |        | (_)      "
+print "| |_  | | ___   _| |_ _ __  "
+print "|  _| | |/ / | | | | | '_ \ "
+print "| |   |   <| |_| | | | | | |"
+print "\_|   |_|\_\\__, |_|_|_| |_|"
+print "             __/ |          "
+print "            |___/           "
+print "---------------------------------------------"
